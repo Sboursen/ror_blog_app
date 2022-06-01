7aa9d87382f14e8419bb4327c5354ad9f2893750
sboursen <dev.sboursen@gmail.com>
2022-06-01 14:19:34 +0100

    Add spec for users#show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns the correct response' do
-      get '/users/index'
+      get '/users'
+      expect(response).to have_http_status(200)
+      expect(response.body).to include('Users')
+      expect(response.body).to include('Lists all users')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns the correct response' do
+      get '/users/show'
       expect(response).to have_http_status(200)
       expect(response.body).to include('Users')
       expect(response.body).to include('Shows info for a specific user')
