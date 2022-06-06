bb929db05264644008891df2c4bc0901f450f529
sboursen <dev.sboursen@gmail.com>
2022-06-06 17:35:35 +0100

    Create Post Methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User',
-                      foreign_key: 'author_id'
+                      foreign_key: 'author_id',
+                      counter_cache: :posts_counter
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  
+  after_save do
+    author.update(posts_counter: author.posts.size)
+  end
+
+  def most_recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
