622d48c28780adb71d12834e10cbaaba7872eb57
sboursen <dev.sboursen@gmail.com>
2022-06-06 18:19:23 +0100

    Add comments and likes counter methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User',
                       foreign_key: 'author_id'
   belongs_to :post, counter_cache: :comments_count
+
+  after_save :update_comments_count
+
+  private
+
+  def update_comments_count
+    post.update(comments_count: post.comments.size)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,15 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  after_save do
-    author.update(posts_counter: author.posts.size)
-  end
+  after_save :update_posts_count
 
   def most_recent_five_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_count
+    author.update(posts_counter: author.posts.size)
   end
 end
