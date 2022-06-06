58ac97f922a6ab89950b3b4cb2227cc2682bc18b
sboursen <dev.sboursen@gmail.com>
2022-06-06 17:36:10 +0100

    Create Comment and Like methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User',
                       foreign_key: 'author_id'
-  belongs_to :post
+  belongs_to :post, counter_cache: :comments_count
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User',
                       foreign_key: 'author_id'
-  belongs_to :post
+  belongs_to :post, counter_cache: :likes_count
 end
